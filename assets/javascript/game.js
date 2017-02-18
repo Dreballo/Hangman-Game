8ba0d0a75fec3f0e7859766c8d89d4ecef30e834
Andre Seballlo <andreseballlo@Stacys-MacBook-Air.local>
2017-02-18 12:47:53 -0500

    fixed logic for game js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,86 +1,125 @@
-
-
 //psuedo code steps: 
 
-	//1. create word list within an array
-	//2. create function to log keystrokes
-	//3. check if letter is already pressed and display guess
-	//4. compare keystrokes to the word [true/false]
-	//5. change the number of guesses left
-	//6. update dom correct guess or incorrect guess)
-	//7. check if they have any guesses left
-			//if yes - what for key up (do it all again)
-			//if no  - ‘game over’
+//1. create word list within an array [complete]
+//2. create function to log keystrokes [complete]
+//3. check if letter is already pressed and display guess [pending]
+//4. compare keystrokes to the word [true/false] -[complete]
+//5. change the number of guesses left [incomplete]
+//6. update dom correct guess or incorrect guess)
+//7. check if they have any guesses left [complete]
+//if yes - what for key up (do it all again)
+//if no  - ‘game over’
 
-	//8. check if they won - start over
+//8. check if they won - start over
+
+var randomNumber = function(length) {
+	return Math.floor(Math.random() * length);
+};
 
 
 
 $(document).ready(function() {
 
-	//word bank for basketball terms
+    //word bank for basketball terms
 
 
-	var wordArray = ['ball','hoop','jersey','rebound','backboard','steal','slam dunk',
-	 		'alley oop', 'free throw', 'jump shot', 'defense'];
+    var wordArray = ['ball', 'hoop', 'jersey', 'rebound', 'backboard', 'steal', 'slam dunk',
+        'alley oop', 'free throw', 'jump shot', 'defense'
+    ];
+
+    var $challengeWord = $('#challenge-word');
 
 
 
-	//variables for collecting data
+    //variables for collecting data
+    var challengeWord = wordArray[ randomNumber(wordArray.length) ];
+    var remainingLetters = challengeWord.length;
+    var userGuess;
+    //var storedGuess = [];
+    //var storedUGuess = [];
+    var fouls = 0;
+    var points = 0;
+    //var counter;
+    //var space;
 
-	var userGuess;
-	//var storedGuess = [];
-	//var storedUGuess = [];
-	var lives = 5;
-	var wins = 0;
-	//var counter;
-	//var space;
+    //function for selecting challenge word from bank for user to guess with keystrokes
 
-	//function for selecting challenge word from bank for user to guess with keystrokes
+    $("#reset").on("click",function(){
+    	fouls=0;
+    	console.log(challengeWord);
+    	challengeWord = wordArray[randomNumber(wordArray.length)];
+    	console.log(challengeWord);
+    });
 
-			$("#reset").on("click",function(){
-
-			var challengeWord = wordArray[(Math.floor(Math.random() * wordArray.length))]; 
 			
-			console.log(challengeWord);
+   
 
-			
+    //for loop to replacing the challenge word with "-" spaces
+    console.log(challengeWord);
 
-	//for loop for replacing the challenge word with "-" spaces
+    var answerArray = [];
 
-			
-			var answerArray = [];
+    for (i = 0; i < challengeWord.length; i++) {
 
-			for(i = 0; i < wordArray.length; i++){
+        answerArray[i] = "-";
 
-				answerArray[i] = "-";
-
-				$(".game-container").append(answerArray[i]);
-			}
-
-			var remainingLetters = wordArray.length;
-
-			});
-
-	//capturing user keystrokes for game input
-	
-		document.onkeyup = function(event) {
+        $challengeWord.append(answerArray[i]);
 
 
-			userGuess = event.key;
-
-			if(userGuess.length > 0) {
+    }
 
 
 
-			}
+    //capturing user keystrokes for game input
+    //while(remainingLetters > 0) {
 
-	
+    //answerArray.join("");
 
-		console.log(event);		
+    //}
+    console.log(challengeWord)
+
+    document.onkeyup = function(event) {
+
+        userGuess = event.key.toLowerCase();
+
+        $challengeWord.empty();
+
+        if(challengeWord.indexOf(userGuess) == -1) {
+        		fouls++;
+        }
+
+        if(fouls > 5){
+        	alert("GAME OVER");
+
+        	return;
+        }
+
+        for (var j = 0; j < challengeWord.length; j++) {
+
+            if (challengeWord[j] === userGuess) {
+
+                answerArray[j] = userGuess;
+
+                remainingLetters--;
+            }
+
+        }
+
+        $challengeWord.append(answerArray.join(""));
+        console.log($challengeWord.text(), challengeWord);
+
+        if ($challengeWord.text() == challengeWord){
+
+        	points++;
+        	alert("You scored: " + points);
+        }
+
+    }
 
 
-		};
+
+
+    //User gets 5 fouls and game over on the 6th.
 
 
 
